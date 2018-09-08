e392b4e9aa6b325189bb05c47998d97fee14126f
xaxwtf <xaxwtf@gmail.com>
2018-09-08 00:02:37 -0300

    2do commit

--- TP_1A Programacion 1/main.c
+++ TP_1A Programacion 1/main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "S_Ariel.h"
-#define TAM 3
+#define TAM 4
 
 int main()
 {
     int opcion= 20;
     char sub_op={'z'};
-    float num=0;
+    float num;
     float num2;
     int resl=-1;
-    int aux;
-    float Valores[TAM];
-    int resFac[1];
+    float valores[TAM];
+    float factorA=1;
+    float factorB=1;
     int estado[5];
+    int i;
     num2=0;
+    num=0;
     inizArray(estado,5,-1,0);
-    inizArray(resFac,1,1,0);
     do{
         printf("---CALCULADORA V 1.0--- \n 1-Ingresar 1er Operando(A=  %.2f). \n 2-Ingresar 2do Operando(B=  %.2f).\n 3-Calcular Todas las Operaciones.\n 4-Informar Resultados.\n 5-Salir \n ",num,num2);
         scanf("%d",&opcion);
@@ -41,58 +42,57 @@ int main()
                         switch(sub_op)
                         {
                         case 'a':
-                            Valores[0]=num+num2;
+                            valores[0]=num+num2;
                             estado[0]=1;
                             break;
                         case 'b':
-                            Valores[1]=num-num2;
+                            valores[1]=num-num2;
                             estado[1]=1;
                             break;
                         case 'c':
                             if(num2!=0)
                             {
-                                Valores[2]=num/num2;
+                                valores[2]=num/num2;
                                 estado[2]=1;
                             }
                             break;
                         case 'd':
-                            Valores[3]=num*num2;
+                            valores[3]=num*num2;
                             estado[3]=1;
                             break;
                         case 'e':
-                            aux=(int)num;
                             if(num==0)
                             {
-                                resFac[0]=1;
+                                factorA=1;
                                 estado[4]=1;
                             }
-                            else if(num<0||num-aux!=0)
+                            else if(num<0)
                             {
-                                resFac[1]=0;
+                                factorA=0;
                             }
                             else
                             {
                             for(int i=num;i>=1;i--)
                                 {
-                                    resFac[0]=resFac[0]*i;
+                                    factorA=factorA*i;
                                 }
                                 estado[4]=1;
                             }
-                            aux=(int)num2;
+
                             if(num2==0)
                             {
-                                resFac[1]=1;
+                                factorB=1;
                                 estado[5]=1;
                             }
-                            else if(num2<0||num2-aux!=0)
+                            else if(num2<0)
                             {
-                                resFac[1]=0;
+                                factorB=0;
                             }
                             else
                             {
-                            for(int i=num2;i>=1;i--)
+                            for(i=num2;i>=1;i--)
                                 {
-                                    resFac[1]=resFac[1]*i;
+                                    factorB=factorB*i;
                                 }
                                 estado[5]=1;
                             }
@@ -112,7 +112,7 @@ int main()
                         case 'a':
                             if(estado[0]!=-1)
                             {
-                                printf("El resultado de la Suma A+B es: %.2f",Valores[0]);
+                                printf("El resultado de la Suma A+B es: %.2f",valores[0]);
                             }
                             else{
                                 printf("No hay Resultados");
@@ -121,7 +121,7 @@ int main()
                         case 'b':
                             if(estado[1]!=-1)
                             {
-                                printf("El resultado de la resta A-B es: %.2f",Valores[1]);
+                                printf("El resultado de la resta A-B es: %.2f",valores[1]);
                             }
                             else{
                                 printf("No hay Resultados");
@@ -130,7 +130,7 @@ int main()
                         case 'c':
                             if(estado[2]!=-1)
                             {
-                                printf("\n El resultado de la Divicion A/B es: %.2f",Valores[2]);
+                                printf("\n El resultado de la Divicion A/B es: %.2f",valores[2]);
                             }
                             else{
                                 printf("\n No se Puede Dividir por Cero");
@@ -139,7 +139,7 @@ int main()
                         case 'd':
                             if(estado[3]!=-1)
                             {
-                                printf("\n El resultado de la Multiplicacion A*B es: %.2f",Valores[3]);
+                                printf("\n El resultado de la Multiplicacion A*B es: %.2f",valores[3]);
                             }
                             else{
                                 printf("\n No hay Resultados");
@@ -148,14 +148,14 @@ int main()
                         case 'e':
                             if(estado[4]!=-1)
                             {
-                                printf("\n El Factorial de A es: %.2f",resFac[0]);
+                                printf("\n El Factorial de A es: %.2f",factorA);
                             }
                             else{
                                 printf("\n no Existe el Factorial de Numeros Negativos o Fracciones ");
                             }
                             if(estado[5]!=-1)
                             {
-                                printf("  El Factorial de B es: %.2f",resFac[1]);
+                                printf("  El Factorial de B es: %.2f",factorB);
                             }
                             else
                             {
